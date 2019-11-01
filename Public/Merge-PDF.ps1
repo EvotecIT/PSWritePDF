25c982676058ee042ae5667136d137e6447ca177
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-11-01 21:19:55 +0100

    Update

--- Public/Merge-PDF.ps1
+++ Public/Merge-PDF.ps1
@@ -5,16 +5,31 @@
         [string] $OutputFile
     )
 
-    [iText.Kernel.Pdf.PdfWriter] $Writer = [iText.Kernel.Pdf.PdfWriter]::new($OutputFile)
-    [iText.Kernel.Pdf.PdfDocument] $PDF = [iText.Kernel.Pdf.PdfDocument]::new($Writer);
-    [iText.Kernel.Utils.PdfMerger] $Merger = [iText.Kernel.Utils.PdfMerger]::new($pdf)
+    if ($OutputFile) {
+        [iText.Kernel.Pdf.PdfWriter] $Writer = [iText.Kernel.Pdf.PdfWriter]::new($OutputFile)
+        [iText.Kernel.Pdf.PdfDocument] $PDF = [iText.Kernel.Pdf.PdfDocument]::new($Writer);
+        [iText.Kernel.Utils.PdfMerger] $Merger = [iText.Kernel.Utils.PdfMerger]::new($PDF)
 
-    foreach ($File in $InputFile) {
-        #//Add pages from the first document
-        $Source = [iText.Kernel.Pdf.PdfReader]::new($File)
-        [iText.Kernel.Pdf.PdfDocument] $SourcePDF = [iText.Kernel.Pdf.PdfDocument]::new($Source);
-        $null = $Merger.merge($SourcePDF, 1, $SourcePDF.getNumberOfPages())
-        $SourcePDF.close()
+        foreach ($File in $InputFile) {
+            if ($File -and (Test-Path -LiteralPath $File)) {
+                try {
+                    $Source = [iText.Kernel.Pdf.PdfReader]::new($File)
+                    [iText.Kernel.Pdf.PdfDocument] $SourcePDF = [iText.Kernel.Pdf.PdfDocument]::new($Source);
+                    $null = $Merger.merge($SourcePDF, 1, $SourcePDF.getNumberOfPages())
+                    $SourcePDF.close()
+                } catch {
+                    $ErrorMessage = $_.Exception.Message
+                    Write-Warning "Merge-PDF - Processing document $File failed with error: $ErrorMessage"
+                }
+            }
+        }
+        try {
+            $PDF.Close()
+        } catch {
+            $ErrorMessage = $_.Exception.Message
+            Write-Warning "Merge-PDF - Saving document $OutputFile failed with error: $ErrorMessage"
+        }
+    } else {
+        Write-Warning "Merge-PDF - Output file was empty. Please give a name to file. Terminating."
     }
-    $PDF.Close()
 }
