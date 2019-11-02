2efcb898682e5ccb3cde6ebf445fc58969bd9245
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-11-02 11:33:46 +0100

    Updates

--- Example/Example04.ExtractText/Example04.ps1
+++ Example/Example04.ExtractText/Example04.ps1
@@ -1,3 +1,7 @@
 ﻿Import-Module .\PSWritePDF.psd1 -Force
 
+# Get all pages text
 Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf"
+
+# Get page 1 text only
+Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf" -Page 1
--- Public/Convert-PDFToText.ps1
+++ Public/Convert-PDFToText.ps1
@@ -2,22 +2,28 @@
     [CmdletBinding()]
     param(
         [string] $FilePath,
-        [int[]] $Page,
-        [switch] $All
+        [int[]] $Page
     )
     if ($FilePath -and (Test-Path -LiteralPath $FilePath)) {
         $Source = [iText.Kernel.Pdf.PdfReader]::new($FilePath)
         [iText.Kernel.Pdf.PdfDocument] $SourcePDF = [iText.Kernel.Pdf.PdfDocument]::new($Source);
         [iText.Kernel.Pdf.Canvas.Parser.Listener.LocationTextExtractionStrategy] $ExtractionStrategy = [iText.Kernel.Pdf.Canvas.Parser.Listener.LocationTextExtractionStrategy]::new()
 
-        if ($All) {
-            $PagesCount = $SourcePDF.GetNumberOfPages()
+        $PagesCount = $SourcePDF.GetNumberOfPages()
+        if ($Page.Count -eq 0) {
             for ($Count = 1; $Count -le $PagesCount; $Count++) {
                 $ExtractedPage = $SourcePDF.GetPage($Count)
                 [iText.Kernel.Pdf.Canvas.Parser.PdfTextExtractor]::GetTextFromPage($ExtractedPage, $ExtractionStrategy)
             }
         } else {
-
+            foreach ($Count in $Page) {
+                if ($Count -le $PagesCount -and $Count -gt 0) {
+                    $ExtractedPage = $SourcePDF.GetPage($Count)
+                    [iText.Kernel.Pdf.Canvas.Parser.PdfTextExtractor]::GetTextFromPage($ExtractedPage, $ExtractionStrategy)
+                } else {
+                    Write-Warning "Convert-PDFToText - File $FilePath doesn't contain page number $Count. Skipping."
+                }
+            }
         }
 
     } else {
