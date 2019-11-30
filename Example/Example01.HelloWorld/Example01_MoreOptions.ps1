74ea7947e4b97c76fda2778036423585cf47edae
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-11-30 21:13:48 +0100

    Examples

--- Example/Example01.HelloWorld/Example01_MoreOptions.ps1
+++ Example/Example01.HelloWorld/Example01_MoreOptions.ps1
@@ -1,6 +1,6 @@
 ﻿Import-Module .\PSWritePDF.psd1 -Force
 
-New-PDF  -MarginLeft 120 -MarginRight 20 -MarginTop 20 -MarginBottom 20 -PageSize B2 -Rotate {
+New-PDF  -MarginLeft 120 -MarginRight 20 -MarginTop 20 -MarginBottom 20 -PageSize A4 -Rotate {
     New-PDFText -Text 'Test ', 'Me', 'Oooh' -FontColor BLUE, YELLOW, RED
     New-PDFList -Indent 3 {
         New-PDFListItem -Text 'Test'
--- Example/Example01.HelloWorld/Example01_WithSectionsMix.ps1
+++ Example/Example01.HelloWorld/Example01_WithSectionsMix.ps1
@@ -1,6 +1,6 @@
-﻿Import-Module .\PSWritePDF.psd1 -Force
+#Import-Module .\PSWritePDF.psd1 -Force
 
-New-PDF -Rotate {
+New-PDF -PageSize A4 -Rotate {
     New-PDFText -Text 'Hello ', 'World' -Font HELVETICA, TIMES_ITALIC -FontColor GRAY, BLUE -FontBold $true, $false, $true
     New-PDFText -Text 'Testing adding text. ', 'Keep in mind that this works like array.' -Font HELVETICA -FontColor RED
     New-PDFText -Text 'This text is going by defaults.', ' This will continue...', ' and we can continue working like that.'
