fe107eb37c700483121001aea2d16495a9140919
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-05-02 18:50:17 +0200

    Rerun examples

--- Example/Example02.Splitting/Example02.ps1
+++ Example/Example02.Splitting/Example02.ps1
@@ -1,3 +1,5 @@
-﻿Import-Module PSWritePDF -Force
+﻿Import-Module .\PSWritePDF.psd1 -Force
 
 Split-PDF -FilePath "$PSScriptRoot\SampleToSplit.pdf" -OutputFolder "$PSScriptRoot\Output"
+
+Split-PDF -FilePath "\\ad1\c$\SampleToSplit.pdf" -OutputFolder "\\ad1\c$\Output"
