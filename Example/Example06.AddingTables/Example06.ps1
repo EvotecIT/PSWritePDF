0cbdf9a15daed243138448251280f6eeeb5bac6f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-21 19:09:41 +0200

    Updated example #7

--- Example/Example06.AddingTables/Example06.ps1
+++ Example/Example06.AddingTables/Example06.ps1
@@ -1,18 +1,20 @@
 ﻿Import-Module .\PSWritePDF.psd1 -Force
 
-$DataTable1 = @(
+$DataTable123 = @(
     [PSCustomObject] @{ Test = 'Name'; Test2 = 'Name2'; Test3 = 'Name3' }
     [PSCustomObject] @{ Test = 'Name'; Test2 = 'Name2'; Test3 = 'Name3' }
     [PSCustomObject] @{ Test = 'Name'; Test2 = 'Name2'; Test3 = 'Name3' }
     [PSCustomObject] @{ Test = 'Name'; Test2 = 'Name2'; Test3 = 'Name3' }
 )
 
-$DataTable2 = @(
+$DataTable1234 = @(
     [ordered] @{ Test = 'Name'; Test2 = 'Name2'; Test3 = 'Name3' }
     [ordered] @{ Test = 'Name'; Test2 = 'Name2'; Test3 = 'Name3' }
     [ordered] @{ Test = 'Name'; Test2 = 'Name2'; Test3 = 'Name3' }
     [ordered] @{ Test = 'Name'; Test2 = 'Name2'; Test3 = 'Name3' }
 )
+
+$DataTable12345 = [ordered] @{ Test = 'Name'; Test2 = 'Name2'; Test3 = 'Name3' }
 
 New-PDF {
     New-PDFText -Text 'Hello ', 'World' -Font HELVETICA, TIMES_ITALIC -FontColor GRAY, BLUE -FontBold $true, $false, $true
@@ -21,7 +23,7 @@ New-PDF {
 
     New-PDFText -Text 'This table is representation of ', 'PSCustomObject', ' or other', ', ', 'standard types' -FontColor BLACK, RED, BLACK, BLACK, RED -FontBold $false, $true, $false, $false, $true
 
-    New-PDFTable -DataTable $DataTable1
+    New-PDFTable -DataTable $DataTable123
 
     New-PDFText -Text 'This shows how to create a list' -FontColor MAGENTA
 
@@ -30,8 +32,12 @@ New-PDF {
         New-PDFListItem -Text '2nd'
     }
 
+    New-PDFText -Text 'This table is representation of ', 'Array of Hashtable/OrderedDictionary' -FontColor BLACK, BLUE
+
+    New-PDFTable -DataTable $DataTable1234
+
     New-PDFText -Text 'This table is representation of ', 'Hashtable/OrderedDictionary' -FontColor BLACK, BLUE
 
-    New-PDFTable -DataTable $DataTable2
+    New-PDFTable -DataTable $DataTable12345
 
 } -FilePath "$PSScriptRoot\Example06.pdf" -Show
