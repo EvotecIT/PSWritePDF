27d5b604ab7d1d81084ba9466110aee5cdb3614b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-09 10:44:47 +0100

    Example update

--- Example/Example01.HelloWorld/Example01_Simple.ps1
+++ Example/Example01.HelloWorld/Example01_Simple.ps1
@@ -1,6 +1,7 @@
 ﻿Import-Module .\PSWritePDF.psd1 -Force
 
 New-PDF {
+    Register-PDFFont -FontName 'Verdana' -FontPath 'C:\Windows\fonts\verdana.ttf' -Encoding IDENTITY_H -Cached -Default
     New-PDFText -Text 'Hello ', 'World' -Font HELVETICA, TIMES_ITALIC -FontColor GRAY, BLUE -FontBold $true, $false, $true
     New-PDFText -Text 'Testing adding text. ', 'Keep in mind that this works like array.' -Font HELVETICA -FontColor RED
     New-PDFText -Text 'This text is going by defaults.', ' This will continue...', ' and we can continue working like that.'
@@ -12,8 +13,13 @@ New-PDF {
     New-PDFText -Text 'Hello ', 'World' -Font HELVETICA, TIMES_ITALIC -FontColor GRAY, BLUE -FontBold $true, $false, $true
     New-PDFText -Text 'Testing adding text. ', 'Keep in mind that this works like array.' -Font HELVETICA -FontColor RED
     New-PDFText -Text 'This text is going by defaults.', ' This will continue...', ' and we can continue working like that.'
+    New-PDFText -Text 'Following text written in polish with default font Verdana that we set in the beggining: '
+    New-PDFText -Text 'Polski tekst ma się dobrze, tak dobrze, że aż za dobrze'
+    New-PDFText -Text 'łomatko' -Font 'HELVETICA'
     New-PDFList -Indent 3 {
         New-PDFListItem -Text 'Test'
         New-PDFListItem -Text '2nd'
+        New-PDFListItem -Text 'ąłęć - unicode with Verdana must work', ' and bold' -FontBold $null, $true -Font Verdana, HELVETICA -FontColor GRAY, BLUE
+        New-PDFListItem -Text 'ąłęć - must work', ' and bold'
     }
 } -FilePath "$PSScriptRoot\Example01_Simple.pdf" -Show
