adb2c23707d676aaf59befc43a87d1af8d98f613
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-11 16:51:12 +0100

    Examples updates

---
+++ Example/Example01.HelloWorld/Example01_Simple3.ps1
@@ -0,0 +1,18 @@
+﻿Import-Module .\PSWritePDF.psd1 -Force
+
+# This shows handling of edge cases where this shouldnt throw errors, but warnings.
+
+New-PDF -FilePath "$PSScriptRoot\Example01_Simple3-1.pdf" -PDFContent {
+    Register-PDFFont -FontName 'Verdana' -FontPath 'C:\Windows\fonts\verdana.ttf' -Encoding IDENTITY_H -Cached -Default
+    New-PDFText -Text 'Hello ', 'Привет !'
+} -Show
+New-PDF -FilePath "$PSScriptRoot\Example01_Simple3-2.pdf" -PDFContent {
+    Register-PDFFont -FontName 'Verdana' -FontPath 'C:\Windows\fonts\verdana.ttf' -Encoding IDENTITY_H -Cached -Default
+    New-PDFText -Text 'Hello ', 'Привет !'
+} -Show
+
+New-PDF -FilePath "$PSScriptRoot\Example01_Simple3-3.pdf" -PDFContent {
+
+}
+
+New-PDF -FilePath "D:\Example01_Simple2.pdf" -PDFContent { 'Test' }
---
+++ Example/Example01.HelloWorld/Example01_Simple4.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module .\PSWritePDF.psd1 -Force
+
+New-PDF -FilePath "D:\Example01_Simple4.1.pdf" -PDFContent { New-PDFText -Text 'Hello ', 'Привет !' } -Show
+New-PDF -FilePath "D:\Example01_Simple4.2.pdf" -PDFContent {
+    Register-PDFFont -FontName 'Verdana' -FontPath 'C:\Windows\fonts\verdana.ttf' -Encoding IDENTITY_H -Cached -Default
+    New-PDFText -Text 'Hello ', 'Привет !'
+} -Show
