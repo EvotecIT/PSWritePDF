4930a55a994da158d371484d4503ecf64dd4f126
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-17 23:29:52 +0100

    Added example

---
+++ Example/Example08.Forms/Example.Forms.ps1
@@ -0,0 +1,18 @@
+﻿Import-Module .\PSWritePDF.psd1 -Force
+
+$FilePath = [IO.Path]::Combine("$PSScriptRoot", "Output", "SampleAcroFormOutput.pdf")
+$FilePathSource = [IO.Path]::Combine("$PSScriptRoot", "Input", "SampleAcroForm.pdf")
+
+$FieldNameAndValueHashTable = [ordered] @{
+    "Text 1" = "Text 1 input"
+    "Text 2" = "Text 2 input"
+    "Text 3" = "Text 3 input"
+}
+
+Set-PDFForm -SourceFilePath $FilePathSource -DestinationFilePath $FilePath -FieldNameAndValueHashTable $FieldNameAndValueHashTable
+
+# Reading PDF forms
+$PDF = Get-PDF -FilePath $FilePath
+$AcrobatFormFields = Get-PDFFormField -PDF $PDF
+$AcrobatFormFields | Format-Table
+Close-PDF -Document $PDF
