b944237c2d15cb9289d29e6a760fb352aae5de3c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-12-20 20:34:41 +0100

    Updated example

--- Example/Example04.ExtractText/Example04.ps1
+++ Example/Example04.ExtractText/Example04.ps1
@@ -1,7 +1,7 @@
-﻿#Import-Module .\PSWritePDF.psd1 -Force
+﻿Import-Module .\PSWritePDF.psd1 -Force
 
 # Get all pages text
 Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf"
 
 # Get page 1 text only
-Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf" -Page 1
+Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf" -Page 1 -IgnoreProtection
