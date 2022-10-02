df9116dc7a4782fe7d55eb1151ec1580b295dd6d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-10-02 19:24:22 +0200

    Update example

--- Example/Example04.ExtractText/Example04.ps1
+++ Example/Example04.ExtractText/Example04.ps1
@@ -3,5 +3,9 @@
 # Get all pages text
 Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf"
 
+Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf" -ExtractionStrategy LocationTextExtractionStrategy
+
+Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf" -ExtractionStrategy SimpleTextExtractionStrategy
+
 # Get page 1 text only
 Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf" -Page 1 -IgnoreProtection
