744b0c47ba3fae83a0dbcb4f0a127ec2b371a130
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-29 17:35:50 +0200

    Update verbose example

--- Example/Example03.Merging/Example03.ps1
+++ Example/Example03.Merging/Example03.ps1
@@ -5,4 +5,4 @@ $FilePath2 = "$PSScriptRoot\Input\OutputDocument1.pdf"
 
 $OutputFile = "$PSScriptRoot\Output\OutputDocument.pdf" # Shouldn't exist / will be overwritten
 
-Merge-PDF -InputFile $FilePath1, $FilePath2 -OutputFile $OutputFile
+Merge-PDF -InputFile $FilePath1, $FilePath2 -OutputFile $OutputFile -Verbose
