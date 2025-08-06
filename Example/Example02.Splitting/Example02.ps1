71aa7eec42c7bba66912ad11e789249fc0b43d7a
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-06 12:42:31 +0200

    Add page range and bookmark splitting

--- Example/Example02.Splitting/Example02.ps1
+++ Example/Example02.Splitting/Example02.ps1
@@ -1,5 +1,11 @@
-﻿Import-Module .\PSWritePDF.psd1 -Force
+Import-Module .\PSWritePDF.psd1 -Force
 
-Split-PDF -FilePath "$PSScriptRoot\SampleToSplit.pdf" -OutputFolder "$PSScriptRoot\Output"
+# Split PDF by fixed page count
+Split-PDF -FilePath "$PSScriptRoot\SampleToSplit.pdf" -OutputFolder "$PSScriptRoot\Output" -SplitCount 1
 
-Split-PDF -FilePath "\\ad1\c$\SampleToSplit.pdf" -OutputFolder "\\ad1\c$\Output"
+# Split PDF by page ranges
+Split-PDF -FilePath "$PSScriptRoot\SampleToSplit.pdf" -OutputFolder "$PSScriptRoot\Output" -PageRange '1-2','3'
+
+# Split PDF using bookmark titles
+Split-PDF -FilePath "$PSScriptRoot\SampleToSplit.pdf" -OutputFolder "$PSScriptRoot\Output" -Bookmark 'Chapter 1','Chapter 2'
+
