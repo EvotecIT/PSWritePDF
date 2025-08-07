7da547e2afd84ca0ee98161683d9bb70bb3f5475
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2025-08-07 20:39:03 +0200

    Output objects from Convert-PDFToText and add OutFile

--- Example/Example04.ExtractText/Example04.ps1
+++ Example/Example04.ExtractText/Example04.ps1
@@ -1,11 +1,16 @@
-﻿Import-Module .\PSWritePDF.psd1 -Force
+Import-Module .\PSWritePDF.psd1 -Force
 
-# Get all pages text
-Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf"
+# Get all pages text as objects
+$pages = Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf"
+$pages | Format-Table -AutoSize
 
+# Save combined text to file
+Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf" -OutFile "$PSScriptRoot\Example04.txt"
+
+# Use different extraction strategies
 Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf" -ExtractionStrategy LocationTextExtractionStrategy
-
 Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf" -ExtractionStrategy SimpleTextExtractionStrategy
 
 # Get page 1 text only
 Convert-PDFToText -FilePath "$PSScriptRoot\Example04.pdf" -Page 1 -IgnoreProtection
+
